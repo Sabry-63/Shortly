6dfb32c47888fcec1e9b718058dca99ac62b3e14
Sabry-63 <sabrry0@gmail.com>
2020-06-06 21:28:19 +0200

    first commit

---
+++ js/min.js
@@ -0,0 +1,9 @@
+$(function () {
+    "use strict";
+    // Start Function Add Class Copyed To Copy Btn
+    $('.btn-search').click(function () {
+        $(this).addClass('copied');
+        $(this).text('Copied')
+    })
+
+});
